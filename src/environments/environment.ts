c63b1493365c7300546e2737a7fe7b5364726f01
bemalgorzata <bemlgorzata@gmail.com>
2022-04-24 22:19:52 +0200

    firebase instalation + home page + task-list page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'todo-app-93ce9',
+    appId: '1:866611904544:web:b47d7fa825fb1fa286fc45',
+    storageBucket: 'todo-app-93ce9.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAA815atWJRBktaddJ6vKf2OSdUt4KB-Iw',
+    authDomain: 'todo-app-93ce9.firebaseapp.com',
+    messagingSenderId: '866611904544',
+  },
   production: false
 };
 
