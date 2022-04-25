f543b0aea712fb2606acbe4bc44d4e6e203e87ca
bemalgorzata <bemlgorzata@gmail.com>
2022-04-25 22:51:30 +0200

    create-task form added

--- projects/add-task/src/lib/adapters/primary/ui/add-task.component.ts
+++ projects/add-task/src/lib/adapters/primary/ui/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'lib-add-task', templateUrl: './add-task.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class AddTaskComponent {
+  readonly createTask: FormGroup = new FormGroup({task: new FormControl()});
 }
